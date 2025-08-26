f5b965c7927367d41c99ca342e93ec73af906bc4
Daksh-ified <bhardwajdakshcodes@gmail.com>
2025-08-26 23:31:25 +0530

    Initial commit - added a new palindrome program

---
+++ palindrome.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, reversed = 0, remainder, original;
+    cout << "Enter a number: ";
+    cin >> n;
+    original = n;
+
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+
+    if (original == reversed)
+        cout << "Palindrome Number";
+    else
+        cout << "Not a Palindrome";
+    return 0;
+}
